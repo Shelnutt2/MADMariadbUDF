cbbfcf6f023e5dcd390ec8acc1bf082be2059a33
Seth Shelnutt <Shelnutt2@gmail.com>
2016-06-29 08:29:52 -0400

    mad_test: Add median test function

--- src/mad_test.cpp
+++ src/mad_test.cpp
@@ -2,6 +2,11 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch.hpp>
 
+TEST_CASE( "Medians are computed", "[med]" ) {
+	std::vector<int> *med_test_1 = new std::vector<int>{1,1,2,2,4,6,9};
+	REQUIRE( Median(med_test_1) == 2 );
+	delete med_test_1;
+}
 
 TEST_CASE( "Median absolute deviations are computed", "[mad]" ) {
 	std::vector<int> *mad_test_1 = new std::vector<int>{1,1,2,2,4,6,9};
