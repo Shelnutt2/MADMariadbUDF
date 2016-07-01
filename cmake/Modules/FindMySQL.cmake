59b145f796aa52b5369dc5694336101e8a37fd41
Seth Shelnutt <Shelnutt2@gmail.com>
2016-07-01 08:12:41 -0400

    Update FindMySQL Cmake Modules to be more robust.
    This uses the cmake module from the mysql c++ connector project.

--- cmake/Modules/FindMySQL.cmake
+++ cmake/Modules/FindMySQL.cmake
@@ -1,113 +1,164 @@
-#--------------------------------------------------------
-# Copyright (C) 1995-2007 MySQL AB
+#   Copyright 2007 - 2008 MySQL AB, 2008 - 2009 Sun Microsystems, Inc.  All rights reserved.
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of version 2 of the GNU General Public License as
-# published by the Free Software Foundation.
-#
-# There are special exceptions to the terms and conditions of the GPL
-# as it is applied to this software. View the full text of the exception
-# in file LICENSE.exceptions in the top-level directory of this software
-# distribution.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc.,
-# 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
-#
-# The MySQL Connector/ODBC is licensed under the terms of the
-# GPL, like most MySQL Connectors. There are special exceptions
-# to the terms and conditions of the GPL as it is applied to
-# this software, see the FLOSS License Exception available on
-# mysql.com.
+#   The MySQL Connector/C++ is licensed under the terms of the GPL
+#   <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most
+#   MySQL Connectors. There are special exceptions to the terms and
+#   conditions of the GPL as it is applied to this software, see the
+#   FLOSS License Exception
+#   <http://www.mysql.com/about/legal/licensing/foss-exception.html>.
 
 ##########################################################################
+INCLUDE (CheckFunctionExists)
+MACRO(_MYSQL_CONFIG VAR _regex _opt)
+		EXECUTE_PROCESS(COMMAND ${MYSQL_CONFIG_EXECUTABLE} ${_opt}
+				OUTPUT_VARIABLE _mysql_config_output
+				)
+		SET(_var ${_mysql_config_output})
+		STRING(REGEX MATCHALL "${_regex}([^ ]+)" _mysql_config_output "${_mysql_config_output}")
+		STRING(REGEX REPLACE "^[ \t]+" "" _mysql_config_output "${_mysql_config_output}")
+		STRING(REGEX REPLACE "[\r\n]$" "" _mysql_config_output "${_mysql_config_output}")
+		STRING(REGEX REPLACE "${_regex}" "" _mysql_config_output "${_mysql_config_output}")
+		SEPARATE_ARGUMENTS(_mysql_config_output)
+		SET(${VAR} ${_mysql_config_output})
+ENDMACRO(_MYSQL_CONFIG _regex _opt)
 
+
+IF (NOT MYSQL_CONFIG_EXECUTABLE AND NOT WIN32)
+	IF (EXISTS "$ENV{MYSQL_DIR}/bin/mysql_config")
+		SET(MYSQL_CONFIG_EXECUTABLE "$ENV{MYSQL_DIR}/bin/mysql_config")
+	ELSE (EXISTS "$ENV{MYSQL_DIR}/bin/mysql_config")
+		FIND_PROGRAM(MYSQL_CONFIG_EXECUTABLE
+			NAMES mysql_config
+			DOC "full path of mysql_config"
+			PATHS	/usr/bin
+				/usr/local/bin
+				/opt/mysql/mysql/bin
+				/usr/local/mysql/bin
+		)
+	ENDIF (EXISTS "$ENV{MYSQL_DIR}/bin/mysql_config")
+ENDIF (NOT MYSQL_CONFIG_EXECUTABLE AND NOT WIN32)
 
 #-------------- FIND MYSQL_INCLUDE_DIR ------------------
-FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
-  /usr/include/mysql
-  /usr/local/include/mysql
-  /opt/mysql/mysql/include
-  /opt/mysql/mysql/include/mysql
-  /opt/mysql/include
-  /opt/local/include/mysql5
-  /usr/local/mysql/include
-  /usr/local/mysql/include/mysql
-  $ENV{ProgramFiles}/MySQL/*/include
-  $ENV{SystemDrive}/MySQL/*/include)
-
+SET(MYSQL_CXXFLAGS "")
+IF(MYSQL_CONFIG_EXECUTABLE AND NOT WIN32)
+	_MYSQL_CONFIG(MYSQL_INCLUDE_DIR "(^| )-I" "--include")
+	MESSAGE(STATUS "mysql_config was found ${MYSQL_CONFIG_EXECUTABLE}")
+	EXECUTE_PROCESS(COMMAND ${MYSQL_CONFIG_EXECUTABLE} "--cflags"
+					OUTPUT_VARIABLE _mysql_config_output
+					)
+	STRING(REGEX MATCHALL "-m([^\r\n]+)" MYSQL_LINK_FLAGS "${_mysql_config_output}")
+	STRING(REGEX REPLACE "[\r\n]$" "" MYSQL_CXXFLAGS "${_mysql_config_output}")
+#	ADD_DEFINITIONS("${MYSQL_CXXFLAGS}")
+ELSE (MYSQL_CONFIG_EXECUTABLE AND NOT WIN32)
+	MESSAGE(STATUS "ENV{MYSQL_DIR} = $ENV{MYSQL_DIR}")
+	FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
+			$ENV{MYSQL_INCLUDE_DIR}
+			$ENV{MYSQL_DIR}/include
+			/usr/include/mysql
+			/usr/local/include/mysql
+			/opt/mysql/mysql/include
+			/opt/mysql/mysql/include/mysql
+			/usr/local/mysql/include
+			/usr/local/mysql/include/mysql
+			$ENV{ProgramFiles}/MySQL/*/include
+			$ENV{SystemDrive}/MySQL/*/include)
+ENDIF (MYSQL_CONFIG_EXECUTABLE AND NOT WIN32)
 #----------------- FIND MYSQL_LIB_DIR -------------------
-IF (WIN32)
-  # Set lib path suffixes
-  # dist = for mysql binary distributions
-  # build = for custom built tree
-  IF (CMAKE_BUILD_TYPE STREQUAL Debug)
-    SET(libsuffixDist debug)
-    SET(libsuffixBuild Debug)
-  ELSE (CMAKE_BUILD_TYPE STREQUAL Debug)
-    SET(libsuffixDist opt)
-    SET(libsuffixBuild Release)
-    ADD_DEFINITIONS(-DDBUG_OFF)
-  ENDIF (CMAKE_BUILD_TYPE STREQUAL Debug)
 
-  FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient
-    PATHS
-    $ENV{MYSQL_DIR}/lib/${libsuffixDist}
-    $ENV{MYSQL_DIR}/libmysql
-    $ENV{MYSQL_DIR}/libmysql/${libsuffixBuild}
-    $ENV{MYSQL_DIR}/client/${libsuffixBuild}
-    $ENV{MYSQL_DIR}/libmysql/${libsuffixBuild}
-    $ENV{ProgramFiles}/MySQL/*/lib/${libsuffixDist}
-    $ENV{SystemDrive}/MySQL/*/lib/${libsuffixDist})
+
+IF (WIN32)
+	# Set lib path suffixes
+	# dist = for mysql binary distributions
+	# build = for custom built tree
+	IF (CMAKE_BUILD_TYPE STREQUAL Debug)
+		SET(libsuffixDist debug)
+		SET(libsuffixBuild Debug)
+	ELSE (CMAKE_BUILD_TYPE STREQUAL Debug)
+		SET(libsuffixDist opt)
+		SET(libsuffixBuild Release)
+		ADD_DEFINITIONS(-DDBUG_OFF)
+	ENDIF (CMAKE_BUILD_TYPE STREQUAL Debug)
+
+	FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient
+				 PATHS
+				 $ENV{MYSQL_DIR}/lib/${libsuffixDist}
+				 $ENV{MYSQL_DIR}/libmysql/${libsuffixBuild}
+				 $ENV{MYSQL_DIR}/client/${libsuffixBuild}
+				 $ENV{ProgramFiles}/MySQL/*/lib/${libsuffixDist}
+				 $ENV{SystemDrive}/MySQL/*/lib/${libsuffixDist})
+	IF(MYSQL_LIB)
+		GET_FILENAME_COMPONENT(MYSQL_LIB_DIR ${MYSQL_LIB} PATH)
+	ENDIF(MYSQL_LIB)
 ELSE (WIN32)
-  FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient_r mysqlclient
-    PATHS
-    /usr/lib/mysql
-    /usr/local/lib/mysql
-    /usr/local/mysql/lib
-    /usr/local/mysql/lib/mysql
-    /opt/local/mysql5/lib
-    /opt/local/lib/mysql5/mysql
-    /opt/mysql/mysql/lib/mysql
-    /opt/mysql/lib/mysql)
+	IF (MYSQL_CONFIG_EXECUTABLE)
+		_MYSQL_CONFIG(MYSQL_LIBRARIES    "(^| )-l" "--libs_r")
+		_MYSQL_CONFIG(MYSQL_LIB_DIR "(^| )-L" "--libs_r")
+
+		EXECUTE_PROCESS(COMMAND ${MYSQL_CONFIG_EXECUTABLE} "--version"
+						OUTPUT_VARIABLE __MYSQL_VERSION
+						)
+		# Test it
+		#SET(__MYSQL_VERSION "4.0.10")
+		# trim the string
+		STRING(REGEX REPLACE "[\r\n]$" "" __MYSQL_VERSION "${__MYSQL_VERSION}")
+		STRING(REGEX MATCHALL "(4.1.[0-9]+|[5-9].[0-9]+.[0-9]+)" MYSQL_VERSION "${__MYSQL_VERSION}")
+
+	ELSE (MYSQL_CONFIG_EXECUTABLE)
+		FIND_LIBRARY(MYSQL_LIB NAMES mysqlclient_r
+					 PATHS
+					 $ENV{MYSQL_DIR}/libmysql_r/.libs
+					 $ENV{MYSQL_DIR}/lib
+					 $ENV{MYSQL_DIR}/lib/mysql
+					 /usr/lib/mysql
+					 /usr/local/lib/mysql
+					 /usr/local/mysql/lib
+					 /usr/local/mysql/lib/mysql
+					 /opt/mysql/mysql/lib
+					 /opt/mysql/mysql/lib/mysql)
+		SET(MYSQL_LIBRARIES mysqlclient_r )
+		IF(MYSQL_LIB)
+			GET_FILENAME_COMPONENT(MYSQL_LIB_DIR ${MYSQL_LIB} PATH)
+		ENDIF(MYSQL_LIB)
+	ENDIF (MYSQL_CONFIG_EXECUTABLE)
 ENDIF (WIN32)
 
-IF(MYSQL_LIB)
-  GET_FILENAME_COMPONENT(MYSQL_LIB_DIR ${MYSQL_LIB} PATH)
-ENDIF(MYSQL_LIB)
 
+
+SET(VERBOSE 1)
 IF (MYSQL_INCLUDE_DIR AND MYSQL_LIB_DIR)
-  SET(MYSQL_FOUND TRUE)
 
-  INCLUDE_DIRECTORIES(${MYSQL_INCLUDE_DIR})
-  LINK_DIRECTORIES(${MYSQL_LIB_DIR})
+	MESSAGE(STATUS "MySQL Include dir: ${MYSQL_INCLUDE_DIR}")
+	MESSAGE(STATUS "MySQL Library    : ${MYSQL_LIB}")
+	MESSAGE(STATUS "MySQL Library dir: ${MYSQL_LIB_DIR}")
+	MESSAGE(STATUS "MySQL CXXFLAGS: ${MYSQL_CXXFLAGS}")
+	MESSAGE(STATUS "MySQL Link flags: ${MYSQL_LINK_FLAGS}")
+	IF (MYSQL_VERSION)
+		MESSAGE(STATUS "MySQL Version: ${MYSQL_VERSION}")
+	ELSE (MYSQL_VERSION)
+		IF(WIN32)
+			#SET(CMAKE_REQUIRED_LIBRARIES ${MYSQL_LIB})
+		ELSE(WIN32)
+			# For now this works only on *nix
+			SET(CMAKE_REQUIRED_LIBRARIES ${MYSQL_LIBRARIES})
+			SET(CMAKE_REQUIRED_INCLUDES ${MYSQL_INCLUDE_DIR})
+			CHECK_FUNCTION_EXISTS("mysql_set_character_set" HAVE_SET_CHARSET)
+			SET(CMAKE_REQUIRED_LIBRARIES)
+			SET(CMAKE_REQUIRED_INCLUDES)
+		ENDIF(WIN32)
+	ENDIF(MYSQL_VERSION)
 
-  FIND_LIBRARY(MYSQL_ZLIB zlib PATHS ${MYSQL_LIB_DIR})
-  FIND_LIBRARY(MYSQL_TAOCRYPT taocrypt PATHS ${MYSQL_LIB_DIR})
-  IF (MYSQL_LIB)
-    SET(MYSQL_CLIENT_LIBS ${MYSQL_LIB})
-  ELSE()
-    SET(MYSQL_CLIENT_LIBS mysqlclient_r)
-  ENDIF()
-  IF (MYSQL_ZLIB)
-    SET(MYSQL_CLIENT_LIBS ${MYSQL_CLIENT_LIBS} zlib)
-  ENDIF (MYSQL_ZLIB)
-  IF (MYSQL_TAOCRYPT)
-    SET(MYSQL_CLIENT_LIBS ${MYSQL_CLIENT_LIBS} taocrypt)
-  ENDIF (MYSQL_TAOCRYPT)
-  # Added needed mysqlclient dependencies on Windows
-  IF (WIN32)
-    SET(MYSQL_CLIENT_LIBS ${MYSQL_CLIENT_LIBS} ws2_32)
-  ENDIF (WIN32)
+	MESSAGE(STATUS "MySQL Include dir: ${MYSQL_INCLUDE_DIR}")
+	MESSAGE(STATUS "MySQL Library dir: ${MYSQL_LIB_DIR}")
+	MESSAGE(STATUS "MySQL CXXFLAGS: ${MYSQL_CXXFLAGS}")
+	MESSAGE(STATUS "MySQL Link flags: ${MYSQL_LINK_FLAGS}")
 
-  MESSAGE(STATUS "MySQL Include dir: ${MYSQL_INCLUDE_DIR}  library dir: ${MYSQL_LIB_DIR}")
-  MESSAGE(STATUS "MySQL client libraries: ${MYSQL_CLIENT_LIBS}")
-ELSEIF (MySQL_FIND_REQUIRED)
-  MESSAGE(FATAL_ERROR "Cannot find MySQL. Include dir: ${MYSQL_INCLUDE_DIR}  library dir: ${MYSQL_LIB_DIR}")
+	INCLUDE_DIRECTORIES(${MYSQL_INCLUDE_DIR})
+	LINK_DIRECTORIES(${MYSQL_LIB_DIR})
+
+	SET(MYSQL_FOUND TRUE)
+ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIB_DIR)
+	IF(NOT WIN32)
+		MESSAGE(SEND_ERROR "mysql_config wasn't found, -DMYSQL_CONFIG_EXECUTABLE=...")
+	ENDIF(NOT WIN32)
+	MESSAGE(FATAL_ERROR "Cannot find MySQL. Include dir: ${MYSQL_INCLUDE_DIR}  library dir: ${MYSQL_LIB_DIR} cxxflags: ${MYSQL_CXXFLAGS}")
 ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIB_DIR)
