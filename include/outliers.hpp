1bce4c6716515d0296f0a3e4f67d6b66773eafde
Seth Shelnutt <Shelnutt2@gmail.com>
2017-05-29 18:32:18 -0400

    Fix logic for IQR check. #9

--- include/outliers.hpp
+++ include/outliers.hpp
@@ -24,10 +24,7 @@ std::vector<T> remove_outlier(std::vector<T> inData) {
     for(T dataPoint : inData) {
         // We want to exclude all data that is outside the range, and is an outlier.
         // We are excluding things that are < (Q1 - 1.5*IQR) AND > (Q3 + 1.5*IQR)
-      std::cout << "dataPoint: " << dataPoint << " lower: "
-                << quantiles[0] - (1.5 * IQR) << " upper: "
-                << quantiles[1] + (1.5 * IQR) << std::endl;
-        if((dataPoint > (quantiles[0] - (1.5 * IQR))) || (dataPoint < (quantiles[1] + (1.5 * IQR)))) {
+        if((dataPoint > (quantiles[0] - (1.5 * IQR))) && (dataPoint < (quantiles[1] + (1.5 * IQR)))) {
             data.push_back(dataPoint);
         }
     }
